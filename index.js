141f11953817eb2492aa1b1f307082375d5c268f
Heather McLeod <heather.mcleod@mail.utoronto.ca>
2018-08-29 13:52:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Add your functions and code here
+var kittens = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendKitten(name) {
+  kittens.push("Ralph")
+  return kittens
+}
+
+var kittens = ["Milo", "Otis", "Garfield"];
+
+function destructivelyPrependKitten (name) {
+  kittens.unshift("Bob")
+  return kittens
+}
+
+var kittens =["Milo", "Otis", "Garfield"];
+function destructivelyRemoveLastKitten () {
+  kittens.pop()
+  return kittens
+}
+
+var kittens = ["Milo", "Otis", "Garfield"];
+function destructivelyRemoveFirstKitten () {
+  kittens.shift()
+  return kittens
+}
+
+var kittens = ["Milo", "Otis", "Garfield"];
+var kit = ["Broom"]
+function appendKitten(name) {
+var appendKitten = kittens.concat(kit)
+return appendKitten
+}
